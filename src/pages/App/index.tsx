401c92d05710532d4985224b5945d1ec8b6a18e6
Higor Matheus O Vieira <hmoliveira83@gmail.com>
2020-11-17 16:58:22 -0300

    iniciando app

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <h1>tudo certo</h1>
+    </Container>
+  );
+};
+
+export default App;
